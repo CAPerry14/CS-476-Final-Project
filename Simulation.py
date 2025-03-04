c93280d69d942620d325e23da8c7f5414b1a5fff
Cody Perry <perrycod@oregonstate.edu>
2025-03-04 15:33:49 -0800

    Added parameters, node generation, and position generation. We can also alter speed with the mobility module.

--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,24 @@
 from ns import ns
 import sys
+import random
+
+#Parameters
+numVehicles = 2
+defaultXSpeed = 0.0
+defaultYSpeed = 0.0
+gridWidth = 3
+xPositions = [0] * numVehicles
+yPositions = [0] * numVehicles
 
 
+#Setting up vehicles
+nodes = ns.network.NodeContainer()
+nodes.Create(numVehicles)
 
+#Setting up mobility (positions and speeds of vehicles)
+mobility = ns.mobility.MobilityHelper()
+mobility.SetPositionAllocator("ns3::GridPositionAllocator", "MinX", ns.core.DoubleValue(0.0),
+                              "MinY", ns.core.DoubleValue (0.0), "DeltaX", ns.core.DoubleValue(defaultXSpeed), "DeltaY", ns.core.DoubleValue(defaultYSpeed),
+                              "GridWidth", ns.core.UintegerValue(gridWidth), "LayoutType", ns.core.StringValue("RowFirst"))
+mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel")
+mobility.Install(nodes)
